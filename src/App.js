e06f3895b98214a22eba21ac4acc199372788a25
Jaswant Singh <jaswant.sandhu88@gmail.com>
2021-05-30 16:56:15 +0530

    Passing event object.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
-import { useState } from "react"
+import "./App.css";
+import { useState } from "react";
 import Message from "./components/message";
 
 function App() {
+  const [message, setMessage] = useState("Hello World!!!");
 
-  const [message, setMessage] = useState("Hello World!!!")
-
-  function updateMessage(){
-      setMessage("Hello Everyone!!!");
+  function updateMessage() {
+    setMessage("Hello Everyone!!!");
   }
 
   return (
     <>
       <Message message={message} />
-      <button onClick={updateMessage}>Update Message</button>
+      <button
+        onClick={(e) => {
+          updateMessage(e);
+        }}
+      >
+        Update Message
+      </button>
     </>
   );
 }
