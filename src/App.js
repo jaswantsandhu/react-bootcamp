352c86486b241fea0d992e5270df1c97ec689c97
Jaswant Singh <jaswant.sandhu88@gmail.com>
2021-05-30 16:43:57 +0530

    Adding variables as properties.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Message from "./components/message";
 
 function App() {
+
+  const message = 'Hello World!!!'
+
   return (
-    <Message message={'Hello World!!!'} />
+    <Message message={message} />
   );
 }
 
